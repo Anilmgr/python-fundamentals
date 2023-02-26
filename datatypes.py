31b67319223c9790b4d379d9ada2c0e77532dccf
Anil Baral <anilcodes60@gmail.com>
2023-02-26 21:50:32 +0545

    4. Data types

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+x = "Hello World"   # str
+print(type(x))
+x = 20              # int
+print(type(x))
+x = 20.0            # float
+print(type(x))
+x = 1j              # complex
+print(type(x))
+x = ["apple", "banana", "cherry"] # list
+print(type(x))
+x = ("apple", "banana", "cherry") # tuple
+print(type(x))
+x = range(6)                      # range
+print(type(x))
+x = {"name" : "John", "age" : 36} # dict
+print(type(x))
+x = {"apple", "banana", "cherry"} # set
+print(type(x))
+x = frozenset({"apple", "banana", "cherry"}) # forzenset
+print(type(x))
+x = True                          # bool
+print(type(x))
+x = b"Hello"                      # bytes
+print(type(x))
+x = bytearray(5)                  # bytearray
+print(type(x))
+x = memoryview(bytes(5))          # memoryview
+print(type(x))
+x = None                          # NoneType
+print(type(x))
