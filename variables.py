10ebb26ab60a3fff6be58cd9764c05432c4d7983
Anil Baral <anilcodes60@gmail.com>
2023-02-26 20:53:59 +0545

    2. Variables, Casting, Type, Case-sensitive

---
+++ variables.py
@@ -0,0 +1,16 @@
+x = 10
+X = 20 #case sensitive x & X are different variables
+y = float(12) # type casting
+z = "hello world"
+# is the same as
+s = 'hello world'
+# printing sum of x and y 
+print(x+y)
+# printing z (string)
+print(z)
+"""
+This is multiline comment.
+Comment on more than
+one line. 
+"""
+print(type(x),type(y),type(z)) # type shows variable datatype
