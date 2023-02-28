282b1bb562b1602f0700dc4b38f302c89077e484
Anil Baral <anilcodes60@gmail.com>
2023-02-28 09:23:13 +0545

    6. Casting

---
+++ casting.py
@@ -0,0 +1,19 @@
+# int()
+# float()
+# str()
+
+x = int(1)          # value of x is 1
+y = int(2.8)        # value of y is 2
+z = int('3')        # value of z is 3
+print(x, y, z)
+
+w = float(1)        # value of w is 1.0
+x = float(2.8)      # value of x is 2.8
+y = float('3')      # value of y is 3.0
+z = float('4.2')    # value of z is 4.2
+print(w, x, y, z)
+
+x = str('str1')     # value of x is 'str1
+y = str(2)          # value of y is '2'
+z = str(3.0)        # value of z is '3.0'
+print(x, y, z)
