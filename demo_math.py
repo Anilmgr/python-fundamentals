1d8f7c3b99b9b9ac4a84c553ba4f8573e30ea8a2
Anil Baral <anilcodes60@gmail.com>
2023-03-28 21:08:20 +0545

    27. Python Math

---
+++ demo_math.py
@@ -0,0 +1,15 @@
+import math
+
+#inbuilt function
+x = min(1,3,2)
+print(x)
+print(abs(-7.5))
+print(pow(7,2))
+
+#module
+y = math.sqrt(64)
+print(y)
+
+print(math.ceil(1.4))
+print(math.floor(1.4))
+print(math.pi)
